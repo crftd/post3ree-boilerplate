bf175b82c0b5f7b0f8b1e19024a2e3bffa205c10
nemaeska <k.n.yakushev@gmail.com>
2016-08-22 18:58:18 +0300

    Fixed some eslint errors ./universal/containers/dashboard/Dashboard.js

--- universal/containers/dashboard/Dashboard.js
+++ universal/containers/dashboard/Dashboard.js
@@ -1,16 +1,15 @@
 /**
  * Created by nemaeska on 03.03.16.
  */
-import React from 'react'
+import React from 'react';
 
-import styles from './../common/main.pcss'
-
-import ListProjects from '../../components/list-projects/ListProjects'
-import Menu from '../../components/menu/Menu'
+import styles from './../common/main.pcss';
+import ListProjects from '../../components/list-projects/ListProjects';
+import Menu from '../../components/menu/Menu';
 
 export default class Dashboard extends React.Component {
-    render () {
-        return  <div>
+    render() {
+        return <div>
                     <Menu />
                     <ListProjects className={styles.main} />
                 </div>;
