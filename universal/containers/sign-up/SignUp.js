27da411b1c10409c17ba66ba9995a2d0d6819f00
nemaeska <k.n.yakushev@gmail.com>
2016-08-22 19:13:58 +0300

    Fixed some eslint errors ./universal/containers/sign-up/SignUp.js

--- universal/containers/sign-up/SignUp.js
+++ universal/containers/sign-up/SignUp.js
@@ -2,19 +2,18 @@
  * Created by invader on 13.04.16.
  */
 
-import React from 'react'
-import { connect } from 'react-redux'
-import s from './signUp.pcss'
+import React from 'react';
+import { connect } from 'react-redux';
+import FaUser from 'react-icons/lib/fa/user';
+import FaEmail from 'react-icons/lib/fa/envelope';
+import FaLock from 'react-icons/lib/fa/lock';
 
-import { registerUser } from '../../actions/UserActions'
+import s from './signUp.pcss';
+import { registerUser } from '../../actions/UserActions';
+import Menu from '../../components/menu/Menu';
+import { Input } from '../../components/common/forms/Forms';
+import { Button } from '../../components/common/buttons/Buttons';
 
-import Menu from '../../components/menu/Menu'
-import { Input } from '../../components/common/forms/Forms'
-import { Button } from '../../components/common/buttons/Buttons'
-
-import FaUser from 'react-icons/lib/fa/user'
-import FaEmail from 'react-icons/lib/fa/envelope'
-import FaLock from 'react-icons/lib/fa/lock'
 
 class SignUp extends React.Component {
     constructor(props, context) {
@@ -22,20 +21,20 @@ class SignUp extends React.Component {
         this.state = {
             name: this.props.name || '',
             email: this.props.email || '',
-            password: this.props.password || ''
+            password: this.props.password || '',
         };
     }
 
     handleNameChange(e) {
-        this.setState({name: e.target.value});
+        this.setState({ name: e.target.value });
     }
 
     handleEmailChange(e) {
-        this.setState({email: e.target.value})
+        this.setState({ email: e.target.value });
     }
 
     handlePasswordChange(e) {
-        this.setState({password: e.target.value});
+        this.setState({ password: e.target.value });
     }
 
     handleSubmit(e) {
@@ -44,33 +43,52 @@ class SignUp extends React.Component {
             username: this.state.email,
             role: 'common-user',
             name: this.state.name,
-            password: this.state.password}));
+            password: this.state.password }));
     }
 
-    render () {
-        return  <div>
+    render() {
+        return <div>
                     <Menu/>
-
                     <div className={ s.main }>
-
                         <div className={ s.signup }>
                             <h2 className={ s.title }>Sign Up</h2>
-
                             <div className={ s.form }>
                                 <form>
-                                    <Input label="Nickname" icon={ <FaUser/> } placeholder="xxxKOJI9IHxxx" onChange={::this.handleNameChange} value={this.state.name}/>
-                                    <Input label="Login" icon={ <FaEmail/> } placeholder="user@example.com" onChange={::this.handleEmailChange} value={this.state.email}/>
-                                    <Input label="Password" icon={ <FaLock/> } onChange={::this.handlePasswordChange} value={this.state.password} type="password"/>
+                                    <Input
+                                        label="Nickname"
+                                        icon={ <FaUser/> }
+                                        placeholder="xxxKOJI9IHxxx"
+                                        onChange={::this.handleNameChange}
+                                        value={this.state.name}
+                                    />
+                                    <Input
+                                        label="Login"
+                                        icon={ <FaEmail/> }
+                                        placeholder="user@example.com"
+                                        onChange={::this.handleEmailChange}
+                                        value={this.state.email}
+                                    />
+                                    <Input
+                                        label="Password"
+                                        icon={ <FaLock/> }
+                                        onChange={::this.handlePasswordChange}
+                                        value={this.state.password}
+                                        type="password"
+                                    />
                                     <div className={ s.button }>
-                                        <Button type="submit" onClick={::this.handleSubmit}>Register</Button>
+                                        <Button
+                                            type="submit"
+                                            onClick={::this.handleSubmit}
+                                        >
+                                            Register
+                                        </Button>
                                     </div>
                                 </form>
                             </div>
                         </div>
-
                     </div>
-                </div>
+                </div>;
     }
 }
 
-export default connect(() => ({}))(SignUp)
+export default connect(() => ({}))(SignUp);
