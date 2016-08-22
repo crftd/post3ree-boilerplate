be7fb1e5f1f0e1817086f53a8a8a6f92c26ae391
nemaeska <k.n.yakushev@gmail.com>
2016-08-22 19:08:45 +0300

    Fixed some eslint errors ./universal/containers/sign-in/SignIn.js

--- universal/containers/sign-in/SingIn.js
+++ universal/containers/sign-in/SingIn.js
@@ -2,18 +2,17 @@
  * Created by Space Invader on 10.04.2016.
  */
 
-import React from 'react'
-import { connect } from 'react-redux'
-import s from './signIn.pcss'
+import React from 'react';
+import { connect } from 'react-redux';
+import FaEmail from 'react-icons/lib/fa/envelope';
+import FaLock from 'react-icons/lib/fa/lock';
 
-import { loginUser } from '../../actions/UserActions'
+import s from './signIn.pcss';
+import { loginUser } from '../../actions/UserActions';
+import Menu from '../../components/menu/Menu';
+import { Input } from '../../components/common/forms/Forms';
+import { Button } from '../../components/common/buttons/Buttons';
 
-import Menu from '../../components/menu/Menu'
-import { Input } from '../../components/common/forms/Forms'
-import { Button } from '../../components/common/buttons/Buttons'
-
-import FaEmail from 'react-icons/lib/fa/envelope'
-import FaLock from 'react-icons/lib/fa/lock'
 
 class SingIn extends React.Component {
     constructor(props, context) {
@@ -23,44 +22,44 @@ class SingIn extends React.Component {
             password: this.props.password || '',
 
             emailError: false,
-            passwordError: false
+            passwordError: false,
         };
     }
 
     handleEmailChange(e) {
-        this.setState({email: e.target.value})
+        this.setState({ email: e.target.value });
     }
 
     handlePasswordChange(e) {
-        this.setState({password: e.target.value});
+        this.setState({ password: e.target.value });
     }
 
     handleSubmit(e) {
         e.preventDefault();
-
+        const mailRegExp = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z]|digital|xxx)|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
         let errors = false;
         this.setState({
             emailError: false,
-            passwordError: false
+            passwordError: false,
         });
 
-        if (this.state.password == '') {
+        if (this.state.password === '') {
             errors = true;
-            this.setState({passwordError: true});
+            this.setState({ passwordError: true });
         }
 
-        if (!/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z]|digital|xxx)|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i.test(this.state.email)) {
+        if (!mailRegExp.test(this.state.email)) {
             errors = true;
-            this.setState({emailError: true});
+            this.setState({ emailError: true });
         }
 
         if (!errors) {
-            this.props.dispatch(loginUser({username: this.state.email, password: this.state.password}));
+            this.props.dispatch(loginUser({ username: this.state.email, password: this.state.password }));
         }
     }
 
-    render () {
-        return  <div>
+    render() {
+        return <div>
                     <Menu/>
 
                     <div className={ s.main }>
@@ -70,18 +69,41 @@ class SingIn extends React.Component {
 
                             <div className={ s.form }>
                                 <form>
-                                    <Input label="Username" icon={ <FaEmail/> } placeholder="user@example.com" onChange={::this.handleEmailChange} value={this.state.email} error={this.state.emailError} errorMessage="Incorrect username" errorPlacement="right"/>
-                                    <Input label="Password" type="password" icon={ <FaLock/> } onChange={::this.handlePasswordChange} value={this.state.password} error={this.state.passwordError}  errorMessage="Incorrect password" errorPlacement="right"/>
+                                    <Input
+                                        label="Username"
+                                        icon={ <FaEmail/> }
+                                        placeholder="user@example.com"
+                                        onChange={::this.handleEmailChange}
+                                        value={this.state.email}
+                                        error={this.state.emailError}
+                                        errorMessage="Incorrect username"
+                                        errorPlacement="right"
+                                    />
+                                    <Input
+                                        label="Password"
+                                        type="password"
+                                        icon={ <FaLock/> }
+                                        onChange={::this.handlePasswordChange}
+                                        value={this.state.password}
+                                        error={this.state.passwordError}
+                                        errorMessage="Incorrect password"
+                                        errorPlacement="right"
+                                    />
                                     <div className={ s.button }>
-                                        <Button type="submit" onClick={::this.handleSubmit}>Sign In</Button>
+                                        <Button
+                                            type="submit"
+                                            onClick={::this.handleSubmit}
+                                        >
+                                            Sign In
+                                        </Button>
                                     </div>
                                 </form>
                             </div>
                         </div>
 
                     </div>
-                </div>
+                </div>;
     }
 }
 
-export default connect(() => ({}))(SingIn)
+export default connect(() => ({}))(SingIn);
