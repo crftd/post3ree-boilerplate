42466ea0a294d5d69a00df49569e6670b7c4e2da
nemaeska <k.n.yakushev@gmail.com>
2016-08-22 19:16:48 +0300

    Fixed some eslint errors ./universal/reducers/*

--- universal/reducers/dashboard.js
+++ universal/reducers/dashboard.js
@@ -1,7 +1,7 @@
 /**
  * Created by nemaeska on 03.03.16.
  */
-import { LIST_PROJECT_SUCCESS } from '../actions/constants'
+import { LIST_PROJECT_SUCCESS } from '../actions/constants';
 
 function listProjects(state = [{}], action) {
     switch (action.type) {
