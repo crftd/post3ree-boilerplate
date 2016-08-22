53eabeb76ae48867b4841acbb33ae29e92ec73a7
nemaeska <k.n.yakushev@gmail.com>
2016-08-22 18:56:55 +0300

    Fixed some eslint errors ./universal/containers/add-project/AddProject.js

--- universal/containers/add-project/AddProject.js
+++ universal/containers/add-project/AddProject.js
@@ -1,15 +1,15 @@
 /**
  * Created by x22a on 25.02.16.
  */
-import React from 'react'
-import styles from './../common/main.pcss'
+import React from 'react';
 
-import ProjectCreateForm from '../../components/project-create-form/ProjectCreateForm'
-import Menu from '../../components/menu/Menu'
+import styles from './../common/main.pcss';
+import ProjectCreateForm from '../../components/project-create-form/ProjectCreateForm';
+import Menu from '../../components/menu/Menu';
 
 export default class AddProject extends React.Component {
-    render () {
-        return  <div>
+    render() {
+        return <div>
                     <Menu />
                     <ProjectCreateForm className={styles.main}/>
                 </div>;
