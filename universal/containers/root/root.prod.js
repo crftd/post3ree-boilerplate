8d4c36c2da82e20013d43ef918fe11d14a0f7d44
nemaeska <k.n.yakushev@gmail.com>
2016-08-22 19:04:17 +0300

    Fixed some eslint errors ./universal/containers/root/*

--- universal/containers/root/root.prod.js
+++ universal/containers/root/root.prod.js
@@ -3,16 +3,16 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 
 export default class Root extends Component {
-  render() {
-    const { store, routing } = this.props;
-    return (
-      <Provider store={store}>
-        <div>
-          <Router history={browserHistory}>
-            {routing}
-          </Router>
-        </div>
-      </Provider>
-    );
-  }
+    render() {
+        const { store, routing } = this.props;
+        return (
+            <Provider store={store}>
+                <div>
+                    <Router history={browserHistory}>
+                        {routing}
+                    </Router>
+                </div>
+            </Provider>
+        );
+    }
 };
