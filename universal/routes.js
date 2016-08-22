35f5a35f0ee09fa589adb190a686350de71c047d
nemaeska <k.n.yakushev@gmail.com>
2016-08-22 19:17:56 +0300

    Fixed some eslint errors ./universal/routes.js

--- universal/routes.js
+++ universal/routes.js
@@ -5,11 +5,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import Main from './containers/main/Main'
-import Dashboard from './containers/dashboard/Dashboard'
-import AddProject from './containers/add-project/AddProject'
-import SignUp from './containers/sign-up/SignUp'
-import SignIn from './containers/sign-in/SingIn'
+import Main from './containers/main/Main';
+import Dashboard from './containers/dashboard/Dashboard';
+import AddProject from './containers/add-project/AddProject';
+import SignUp from './containers/sign-up/SignUp';
+import SignIn from './containers/sign-in/SingIn';
 
 export default (
     <Route path='/'>
