68ac3eb3bb238089996ebcfeb2f32f670787bed1
nemaeska <k.n.yakushev@gmail.com>
2016-08-22 17:38:17 +0300

    Fixed some eslint errors ./test/reducer_spec.js and some other files

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -1,11 +1,12 @@
 /**
  * Created by hex22a on 16.03.16.
  */
-import { Map, fromJS } from 'immutable'
-import { expect } from 'chai'
+// import { Map, fromJS } from 'immutable';
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
 
-import { ADD_PROJECT_FAIL, LIST_PROJECT_FAIL } from '../universal/actions/constants'
-import Reducers from '../universal/reducers'
+import { ADD_PROJECT_FAIL, LIST_PROJECT_FAIL } from '../universal/actions/constants';
+import Reducers from '../universal/reducers';
 
 describe('reducers', () => {
     it('Foo does not do anything', () => {
